11bd7f047cc09e3184fc3d389c31d2300352fea0
Nek (Maxime Veber) <nek.dev@gmail.com>
2016-07-18 23:51:43 +0200

    Fix connection class

--- src/Server/Connection.php
+++ src/Server/Connection.php
@@ -49,8 +49,7 @@ class Connection
     public function __construct(ConnectionInterface $socketStream, MessageHandlerInterface $messageHandler, ServerHandshake $handshake = null)
     {
         $this->socketStream = $socketStream;
-        $this->socketStream->on('data', [$this, 'handcheck']);
-        $this->socketStream->on('data', [$this, 'message']);
+        $this->socketStream->on('data', [$this, 'processData']);
         $this->socketStream->on('error', [$this, 'error']);
         $this->handler = $messageHandler;
         $this->handshake = $handshake ?: new ServerHandshake;
@@ -59,19 +58,26 @@ class Connection
     /**
      * @param string $data
      */
-    public function message($data)
+    protected function processMessage($data)
     {
-        if (!$this->handshakeDone) {
-            return;
-        }
+
         
         if ($this->currentMessage === null || $this->currentMessage->isComplete()) {
             $this->currentMessage = new Message();
         }
-        
+
         $this->currentMessage->addFrame(new Frame($data));
         if ($this->currentMessage->isComplete()) {
             $this->handler->onData($this->currentMessage->getContent(), $this);
+        }
+    }
+
+    public function processData($data)
+    {
+        if (!$this->handshakeDone) {
+            $this->processHandcheck($data);
+        } else {
+            $this->processMessage($data);
         }
     }
 
@@ -84,20 +90,20 @@ class Connection
         $frame = new Frame();
         $frame->setPayload($data);
         $frame->setOpcode(Frame::OP_TEXT);
-        $this->write($frame->getRawData());
+        $this->socketStream->write($frame->getRawData());
     }
     
     public function error($data)
     {
-        echo "OMG\n" . $data . "\n\n";
+        echo "There is an error : \n" . $data . "\n\n";
     }
 
-    public function processHandcheck($data)
+    protected function processHandcheck($data)
     {
         if ($this->handshakeDone) {
             return;
         }
-        
+
         $request = Request::create($data);
         $this->handshake->verify($request);
         $response = Response::createSwitchProtocolResponse();
--- tests.php
+++ tests.php
@@ -6,4 +6,23 @@ require 'vendor/autoload.php';
 
 $foo = new Websocket(8088);
 
+
+
+class EchoServer implements \Nekland\Woketo\Message\MessageHandlerInterface
+{
+    public function onConnection(\Nekland\Woketo\Server\Connection $connection)
+    {
+    }
+
+    public function onData($data, \Nekland\Woketo\Server\Connection $connection)
+    {
+        $connection->write($data);
+    }
+}
+
+$foo->setMessageHandler(new EchoServer());
+
+
+
+
 $foo->start();
