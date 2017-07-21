ae51ec7c334255e1bf1e03915c6eeb48148446c4
Nek (Maxime Veber) <nek.dev@gmail.com>
2017-07-21 17:47:34 +0200

    Completed autobahn test client

--- tests/client_autobahn.php
+++ tests/client_autobahn.php
@@ -5,15 +5,19 @@ require '../vendor/autoload.php';
 /**
  * Usage:
  *
- * `php client_autobahn.php` will run the complete autobahn client suite.
+ * `php client_autobahn.php`                 will run the complete autobahn client suite.
+ * `php client_autobahn.php 1.1.1,1.1.2`     will run the 2 specified tests.
  */
 
 
 const AGENT = 'Woketo/2.x';
 
+use Nekland\Woketo\Client\WebSocketClient;
 use Nekland\Woketo\Core\AbstractConnection;
 
 $numberOfTests = null;
+$tests = explode(',', $argv[1] ?? []);
+
 class TestMessageHandler extends \Nekland\Woketo\Message\SimpleMessageHandler
 {
     public function onConnection(AbstractConnection $connection)
@@ -32,25 +36,34 @@ class TestMessageHandler extends \Nekland\Woketo\Message\SimpleMessageHandler
     }
 }
 
-$client = new \Nekland\Woketo\Client\WebSocketClient( 'ws://127.0.0.1:9001/getCaseCount', ['prod' => false]);
+if (empty($tests)) {
+    $client = new WebSocketClient('ws://127.0.0.1:9001/getCaseCount', ['prod' => false]);
 
-$client->start(new class extends \Nekland\Woketo\Message\TextMessageHandler {
-    public function onConnection(AbstractConnection $connection)
-    {
-        echo 'Starting test suite' . "\n";
+    $client->start(new class extends \Nekland\Woketo\Message\TextMessageHandler {
+        public function onConnection(AbstractConnection $connection)
+        {
+            echo 'Starting test suite' . "\n";
+        }
+
+        public function onMessage(string $data, AbstractConnection $connection)
+        {
+            echo "Will perform $data tests\n";
+            global $numberOfTests;
+            $numberOfTests = (int) $data;
+        }
+    });
+
+    for ($i = 1; $i <= $numberOfTests; $i++) {
+        $client = new \Nekland\Woketo\Client\WebSocketClient('ws://127.0.0.1:9001/runCase?case=' . $i . '&agent=' . AGENT, ['prod' => false]);
+        $client->start(new TestMessageHandler());
     }
 
-    public function onMessage(string $data, AbstractConnection $connection)
-    {
-        echo "Will perform $data tests\n";
-        global $numberOfTests;
-        $numberOfTests = (int) $data;
-    }
-});
 
-for ($i = 1; $i <= $numberOfTests; $i++) {
-    $client = new \Nekland\Woketo\Client\WebSocketClient('ws://127.0.0.1:9001/runCase?case=' . $i . '&agent=' . AGENT, ['prod' => false]);
-    $client->start(new TestMessageHandler());
+} else {
+    echo "Running tests " . implode(', ', $tests) . "\n";
+    foreach ($tests as $tuple) {
+        (new WebSocketClient('ws://127.0.0.1:9001/runCase?casetuple=' . $tuple . '&agent=' . AGENT, ['prod' => false]))->start(new TestMessageHandler());
+    }
 }
 
 (new \Nekland\Woketo\Client\WebSocketClient('ws://127.0.0.1:9001/updateReports?agent=' . AGENT, ['prod' => false]))->start(new class extends \Nekland\Woketo\Message\TextMessageHandler {
