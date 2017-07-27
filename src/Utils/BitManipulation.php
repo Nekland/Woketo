2df5613adfcddba1884dc2b8f862a095291ab0ce
Nek (Maxime Veber) <nek.dev@gmail.com>
2017-07-27 08:36:42 +0200

    Added usage of unpack

--- src/Utils/BitManipulation.php
+++ src/Utils/BitManipulation.php
@@ -225,30 +225,26 @@ class BitManipulation
      * @param int|null $size  In bytes. This value should always be precise. Be careful if you don't !
      * @return string
      */
-    public static function intToString(int $frame, int $size = null) : string
+    public static function intToBinaryString(int $frame, int $size = null) : string
     {
-        $res = '';
-        $startingBytes = true;
-        for ($i = 8; $i >= 0; $i--) {
-            $code = ($frame & (255 << ($i * 8))) >> ($i * 8);
-
-            // This condition avoid to take care of front zero bytes (that are always present but we should ignore)
-            if ($code !== 0 || !$startingBytes) {
-                $startingBytes = false;
-                $res .= \chr($code);
+        $format = 'J*';
+        if ($size !== null) {
+            switch (true) {
+                case $size <= 2:
+                    $format = 'n*';
+                    break;
+                case $size <= 4:
+                    $format = 'N*';
+                    break;
+                case $size > 4:
+                    $format = 'J*';
+                    break;
             }
         }
+        $res = pack($format, $frame);
 
-        if ($size !== null) {
-            $actualSize = BitManipulation::frameSize($res);
-            if ($size < $actualSize) {
-                $res = \substr($res, $size - $actualSize);
-            } else if ($size > $actualSize) {
-                $missingChars = $size - $actualSize;
-                for ($i = 0; $i < $missingChars; $i++) {
-                    $res = \chr(0) . $res;
-                }
-            }
+        if ($size === null) {
+            $res = ltrim($res, "\0");
         }
 
         return $res;
